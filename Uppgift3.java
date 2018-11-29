cf582f30ba607ef26b7d364fc673c92f1ea38ca7
Lovisa Jansson <lo6788ja.s@student.lu.se>
2018-11-29 10:23:00 +0100

    final

--- Uppgift3.java
+++ Uppgift3.java
@@ -4,33 +4,36 @@ public class Uppgift3 {
 
 	
 	public static void main(String[] args) {
-		
+		//läs in 5 tal, skriv ut summan, medelvärdet samt största talet
 		Scanner upg3 = new Scanner(System.in);
-		int tal;
-		int [] lista = new int [5]; // skapar array med 5 index
+		int nmbr;
+		int [] array = new int [5]; // skapar array med 5 index
 		int i;
 		int sum=0;
-		int max=0; //initialiserar för att kunna använda
+		int max=0; //initialiserar för att kunna använda i loopen
 		
-		for(i = 0; i < lista.length;i++) {
+		for(i = 0; i < array.length;i++) { //upprepas för hur många index som finns i array
 			System.out.println("skriv in tal " + (i+1) );
-			tal = upg3.nextInt();
-			lista[i] = tal; // tal läggs i array, index 0 till 4
+			nmbr = upg3.nextInt();
+			array[i] = nmbr; // nmbr läggs i array, index 0 till 4
 			
-				if (tal > max) {
-					max=tal; //anger nytt värde på max, uppstår ett högre värde än föregående max ersätts det
+				if (nmbr > max) {
+					max=nmbr; //anger nytt värde på max om nmbr är större än max
 				}
 				
 		}
 		
-		for(i=0; i< lista.length; i++) { 
-			sum = sum + lista[i]; //adderar värden i array på varandra och skapar summa
-		}	
-		int average = (sum / lista.length); //beräknar medelvärde
+		for(i=0; i< array.length; i++) { 
+			sum += array[i]; //adderar värden i array på varandra och skapar summa
+		}
+		
+		int average = (sum / array.length); //beräknar medelvärde
 		
 		System.out.println("summan av talen är " + sum);
 		System.out.println("medelvärdet av talen är " + average);
 		System.out.println("största talet är " + max);
+		
+		upg3.close();
 	}
 	
 }
